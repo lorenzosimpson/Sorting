6e921e8be9595e5c65cdcc9d619a9fe3aad17ff5
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-02-18 19:18:57 -0500

    mvp day 2

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -26,27 +26,30 @@ def merge( arrA, arrB ):
         j += 1 
         k += 1
 
-    print(merged_arr)
     return merged_arr
 
-merge([1, 2, 3, 34, 102], [1, 6, 7, 23, 34])
+#merge([1, 2, 3, 34, 102], [1, 6, 7, 23, 34])
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    # 1. 
-
-
-
-
-    
+    if len(arr) <= 1:
+        return arr 
+    midpoint = (len(arr)) // 2
+  
+    left = merge_sort(arr[:midpoint])
+    right = merge_sort(arr[midpoint:])
+   
+    #     
+    arr = merge(left, right)
     return arr
 
+merge_sort([5, 2, 23, 42, 1, 8, 12, 9])
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
-
+    
     return arr
 
 def merge_sort_in_place(arr, l, r): 
